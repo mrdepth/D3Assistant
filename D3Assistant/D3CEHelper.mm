64c11f61f0bc1fd8c797b7015dedbcbcd8e4b938
mrdepth <shimanski.artem@gmail.com>
2013-03-01 08:54:38 +0300

    Fixed crash with unknown items

--- D3Assistant/D3CEHelper.mm
+++ D3Assistant/D3CEHelper.mm
@@ -37,21 +37,32 @@
 + (d3ce::Gear*) addItemFromDictionary:(NSDictionary*) item toHero:(d3ce::Hero*) hero slot:(d3ce::Item::Slot) slot replaceExisting:(BOOL) replaceExisting {
 	try {
 		NSString* itemID = [item valueForKey:@"id"];
-		d3ce::Gear* gear = hero->addItem([itemID cStringUsingEncoding:NSUTF8StringEncoding]);
-		gear->setSlot(slot);
-
-		NSDictionary* attributes = [item valueForKey:@"attributesRaw"];
-		for (NSString* key in [attributes allKeys]) {
-			NSDictionary* attribute = [attributes valueForKey:key];
-			d3ce::Range value;
-			value.min = [[attribute valueForKey:@"min"] floatValue];
-			value.max = [[attribute valueForKey:@"max"] floatValue];
-			gear->getAttribute([key cStringUsingEncoding:NSUTF8StringEncoding])->setValue(value);
+		try {
+			d3ce::Gear* gear = hero->addItem([itemID cStringUsingEncoding:NSUTF8StringEncoding]);
+			gear->setSlot(slot);
+			
+			NSDictionary* attributes = [item valueForKey:@"attributesRaw"];
+			for (NSString* key in [attributes allKeys]) {
+				NSDictionary* attribute = [attributes valueForKey:key];
+				d3ce::Range value;
+				value.min = [[attribute valueForKey:@"min"] floatValue];
+				value.max = [[attribute valueForKey:@"max"] floatValue];
+				gear->getAttribute([key cStringUsingEncoding:NSUTF8StringEncoding])->setValue(value);
+			}
+			
+			for (NSDictionary* gem in [item valueForKey:@"gems"]) {
+				try {
+					gear->addGem([[gem valueForKeyPath:@"item.id"] cStringUsingEncoding:NSUTF8StringEncoding]);
+				}
+				catch(...) {
+					
+				}
+			}
+			return gear;
 		}
-		
-		for (NSDictionary* gem in [item valueForKey:@"gems"])
-			gear->addGem([[gem valueForKeyPath:@"item.id"] cStringUsingEncoding:NSUTF8StringEncoding]);
-		return gear;
+		catch (...) {
+			return NULL;
+		}
 
 	} catch (d3ce::Hero::SlotIsAlreadyFilledException& exception) {
 		if (replaceExisting) {
